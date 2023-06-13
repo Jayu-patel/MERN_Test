f7eaff66a1aa616b9ef6c888f45118cf8ac84ba5
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-06-13 19:47:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const url = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(require('./router/auth'))
@@ -8,4 +9,4 @@ app.get('/',(req,res)=>{
     res.send("hello from home")
 })
 
-app.listen(5000)
+app.listen(url)
