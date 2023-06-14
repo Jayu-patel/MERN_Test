6d20683730a71e7c7b6dfa30b023e651875c7714
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-06-14 11:02:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
-const url = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const cors = require("cors")
 
 app.use(express.json())
 app.use(require('./router/auth'))
-app.use(cors({
-    origin: "https://calm-licorice-9e4677.netlify.app",
-}))
+app.use(cors(
+    {
+        origin: "https://calm-licorice-9e4677.netlify.app/post"
+    }
+))
 
 app.get('/',(req,res)=>{
     res.send("hello from home")
 })
 
-app.listen(url)
+app.listen(port)
